1e53fde651e7efe929b279144b1db640f90c1bbd
iPiranhaa <tanatezp@gmail.com>
2023-07-23 12:17:59 +0700

    feat: handle exception

--- src/apis/getItemDetailByBrowser.ts
+++ src/apis/getItemDetailByBrowser.ts
@@ -2,33 +2,50 @@ import {
   type IData,
   type ISingleItemByBrowserResponse,
 } from '../interfaces/singleItemByBrowserResponse'
+import { PuppeteerLaunchOptions } from 'puppeteer'
 import puppeteer from 'puppeteer-extra'
 import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker'
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
 puppeteer.use(AdblockerPlugin()).use(StealthPlugin())
 
-const getItemDetailByBrowser = (url: string): Promise<IData> => {
-  return new Promise((resolve) => {
-    puppeteer
-      .launch({
-        headless: true,
-        defaultViewport: null,
-      })
-      .then(async (browser) => {
-        const page = await browser.newPage()
-        await page.setRequestInterception(true)
-        await page.goto(url)
+interface IOptions {
+  isHeadless?: boolean
+  executablePath?: string
+}
 
-        page.on('response', async (response) => {
-          const url = response.url()
-          if (url.includes('pdp/get_pc')) {
-            const json: ISingleItemByBrowserResponse = await response.json()
-            resolve(json.data)
-            await browser.close()
-          }
-        })
+const getItemDetailByBrowser = (
+  url: string,
+  options: IOptions = {},
+): Promise<IData> => {
+  const launchOption: PuppeteerLaunchOptions = {
+    defaultViewport: null,
+    headless: true,
+  }
+  if (options.isHeadless !== undefined) {
+    launchOption.headless = options.isHeadless
+  }
+  if (options.executablePath) {
+    launchOption.executablePath = options.executablePath
+  }
+
+  return new Promise((resolve, reject) => {
+    puppeteer.launch(launchOption).then(async (browser) => {
+      const page = await browser.newPage()
+      await page.setRequestInterception(true)
+      await page.goto(url)
+
+      page.on('response', async (response) => {
+        const url = response.url()
+        if (url.includes('pdp/get_pc')) {
+          const json: ISingleItemByBrowserResponse = await response.json()
+          resolve(json.data)
+          await browser.close()
+        } else {
+          reject('Item detail not found.')
+        }
       })
+    })
   })
 }
 
